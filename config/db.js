a26b35b889a140fde9d0ca687cab3488e05beadc
Nirav591 <nirav.v.lathiya@gmail.com>
2025-01-29 14:13:50 +0000

    add

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: 'ls-e2029d66e023eb4cf5366724fe912a2e37ded960.czmq42a8qw3q.eu-west-2.rds.amazonaws.com',
     user: 'dbmasteruser',        // Default MySQL username in XAMPP
     password: 'London1995',        // Default MySQL password is empty in XAMPP
-    database: 'solidblack',  // Your database name
+    database: 'seru',  // Your database name
     port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
